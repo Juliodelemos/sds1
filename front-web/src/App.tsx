e039ae3472b4e9529ad8bef37cc3d340d85569be
Julio Lemos <juliodelemos@gmail.com>
2020-09-19 11:39:56 -0300

    Criando navagacao com rotas

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './Pages/Home';
+import Routes from './Routes';
 
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Home></Home>
-    </div>
-
+    <Routes />
   );
 }
 
